16a37a015b4947afa699148cc40f3f37884ad64c
ShiftSad <rosalimavictor@gmail.com>
2022-06-09 10:37:17 -0300

    Progresso... {😎}

--- src/main/java/me/sadev/campominado/database/game/Arena.java
+++ src/main/java/me/sadev/campominado/database/game/Arena.java
@@ -1,23 +1,33 @@
 package me.sadev.campominado.database.game;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import me.sadev.campominado.CampoMinado;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SplittableRandom;
 
 public class Arena implements Game {
 
     final private Configuration config;
+    final private CampoMinado plugin;
+
+    final private List<Player> players = new ArrayList<>();
 
     // Arena inteira de jogo
     final private Location corner1;
     final private Location corner2;
+
+    // Murinho la
+    final private Location Muro1_1;
+    final private Location Muro1_2;
+
+    // Murinho la
+    final private Location Muro2_1;
+    final private Location Muro2_2;
 
     // Area que sai os bloco
     final private Location area1;
@@ -28,19 +38,28 @@ public class Arena implements Game {
 
     final int delay;
 
+    private Player winner = null;
+
     int round = 0;
 
-    public Arena(Configuration config) {
+    public Arena(Configuration config, CampoMinado plugin) {
         this.config = config;
+        this.plugin = plugin;
 
+        // Pegar as locations da config
         this.corner1 = new Location(Bukkit.getWorld(config.getString("arena.corner1.world")), config.getDouble("arena.corner1.x"), config.getDouble("arena.corner1.y"), config.getDouble("arena.corner1.z"));
         this.corner2 = new Location(Bukkit.getWorld(config.getString("arena.corner2.world")), config.getDouble("arena.corner2.x"), config.getDouble("arena.corner2.y"), config.getDouble("arena.corner2.z"));
         this.area1 = new Location(Bukkit.getWorld(config.getString("arena.corner1.world")), config.getDouble("arena.corner1.x"), config.getDouble("arena.corner1.y"), config.getDouble("arena.corner1.z"));
         this.area2 = new Location(Bukkit.getWorld(config.getString("arena.corner2.world")), config.getDouble("arena.corner2.x"), config.getDouble("arena.corner2.y"), config.getDouble("arena.corner2.z"));
+        this.Muro1_1 = new Location(Bukkit.getWorld(config.getString("arena.Muro1_1.world")), config.getDouble("arena.Muro1_1.x"), config.getDouble("arena.Muro1_1.y"), config.getDouble("arena.Muro1_1.z"));
+        this.Muro1_2 = new Location(Bukkit.getWorld(config.getString("arena.Muro1_2.world")), config.getDouble("arena.Muro1_2.x"), config.getDouble("arena.Muro1_2.y"), config.getDouble("arena.Muro1_2.z"));
+        this.Muro2_1 = new Location(Bukkit.getWorld(config.getString("arena.Muro2_1.world")), config.getDouble("arena.Muro2_1.x"), config.getDouble("arena.Muro2_1.y"), config.getDouble("arena.Muro2_1.z"));
+        this.Muro2_2 = new Location(Bukkit.getWorld(config.getString("arena.Muro2_2.world")), config.getDouble("arena.Muro2_2.x"), config.getDouble("arena.Muro2_2.y"), config.getDouble("arena.Muro2_2.z"));
 
         this.spawn = new Location(Bukkit.getWorld(config.getString("arena.spawn.world")), config.getDouble("arena.spawn.x"), config.getDouble("arena.spawn.y"), config.getDouble("arena.spawn.z"));
         this.delay = config.getInt("arena.delay");
 
+        // Colocar os blocos de vidro.
         getBlocks(area1, area2).forEach(block -> block.setType(Material.GLASS));
     }
 
@@ -69,35 +88,84 @@ public class Arena implements Game {
         return this;
     }
 
+    public Location Muro1_1() {
+        return Muro1_1;
+    }
+
+    public Location Muro1_2() {
+        return Muro1_2;
+    }
+
+    public Location Muro2_1() {
+        return Muro2_1;
+    }
+
+    public Location Muro2_2() {
+        return Muro2_2;
+    }
 
     @Override
     public void start() {
+        // Manda as mensanges de iniciar
+        List<?> list = config.getList("mensagems.StartMSG");
+        list.forEach(msg -> players.forEach(p -> p.sendMessage(ChatColor.translateAlternateColorCodes('&' , msg.toString()))));
 
+
+        Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+
+
+        }, 0, 20L * 20);
     }
 
     @Override
     public void stop() {
+        // Desconectar todos os jogadores
+        players.forEach(this::removePlayer);
 
+        // Reseta a arena
+        getBlocks(area1, area2).forEach(block -> block.setType(Material.GLASS));
+
+        // Da as recompensas
+        if (winner == null) return;
+        List<?> list = config.getList("Recompensas.CampoMinado.Comando");
+        list.forEach(value -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), String.format(value.toString(), winner)));
     }
 
     @Override
     public void removePlayer(Player player) {
-
+        player.sendMessage(String.format(config.getString("mensagems.player.eliminado"), player.getDisplayName()));
+        Bukkit.getServer().dispatchCommand(player, "/spawn");
+        players.remove(player);
     }
 
     @Override
     public void addPlayer(Player player) {
-
+        player.sendMessage(String.format(config.getString("mensagems.player.entrou"), player.getDisplayName()));
+        player.teleport(spawn);
+        players.add(player);
     }
 
     @Override
     public void setWinner(Player player) {
-
+        winner = player;
     }
 
     @Override
     public List<Player> getPlayers() {
-        return null;
+        return new ArrayList<>(players);
+    }
+
+    public void newLevel() {
+        round++;
+
+        List<Block> blocks = getBlocks(area1, area2);
+        SplittableRandom random = new SplittableRandom();
+
+        // Colocar os blocos de vidro.
+        blocks.forEach(block -> {
+            if (random.nextInt(0, 101) <= round) return;
+            block.setType(Material.GLASS);
+        });
     }
 
     private List<Block> getBlocks(Location pos1, Location pos2) {
